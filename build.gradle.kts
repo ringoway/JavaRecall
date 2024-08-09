9a23632e36750fdef782b2e729448dce42342492
Azat <azat.takhautdinov@gmail.com>
2024-08-09 15:01:37 +0500

    Задача Java HashMap: Кэшируем, кэшируем. Добавлено правильное логирование на уровне DEBUG, теперь в консоли пишется логи. Создан сервис генерирующий погоду и обновлен конфигуратор gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     // SLF4J dependencies
     implementation("org.slf4j:slf4j-api:2.0.5")
     implementation("org.projectlombok:lombok:1.18.26")
+    implementation("ch.qos.logback:logback-classic:1.4.6")
     annotationProcessor("org.projectlombok:lombok:1.18.26")
 }
 
