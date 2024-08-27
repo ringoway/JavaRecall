b5aafdea9233da5ae21570dcaf3b5dcc3c966cb4
Azat <azat.takhautdinov@gmail.com>
2024-08-27 22:59:36 +0500

    нерабочие нотификации

---
+++ src/main/java/faangschool/lambda_and_FI/meta/NotificationManager.java
@@ -0,0 +1,16 @@
+package faangschool.lambda_and_FI.meta;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class NotificationManager {
+    private Map<String, Consumer<Notification>> identification = new HashMap<>();
+    public void registerHandler (String type, Consumer<Notification> notificationConsumer) {
+        identification.put(type, notificationConsumer);
+    }
+    public void sendNotification (Notification notification) {
+        Consumer<Notification> notificationConsumer = identification.get(notification.getType());
+        registerHandler(notification.getType(), notificationConsumer);
+    }
+}
